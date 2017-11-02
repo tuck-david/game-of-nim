70392000d190fb77d41db360f13329403e733925
Lil-pump-is-a-religion <33261588+Lil-pump-is-a-religion@users.noreply.github.com>
2017-11-02 12:16:35 -0400

    Update game-of-nim-main.java

--- game-of-nim-main.java
+++ game-of-nim-main.java
@@ -1,1 +1,4 @@
 System.out.println("Welcome to the game of nim, please enter your name and we will explain the rules");
+System.out.println("What is your name?");
+String name_user_1 = Input.nextLine();
+System.out.println("Here are the rules," + name_user_1 + ". The rules are
