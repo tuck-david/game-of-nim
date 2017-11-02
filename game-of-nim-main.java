59f9b4c384dd2eccdb5ee4eecdfc60a9701a8fda
tuck-david <32722002+tuck-david@users.noreply.github.com>
2017-11-02 12:31:54 -0400

    Update game-of-nim-main.java

--- game-of-nim-main.java
+++ game-of-nim-main.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
-class sort2 {
+class game-of-nim-main {
  public static void main(String[] args) {
    Scanner input = new Scanner(System.in);
 System.out.println("Welcome to the game of nim, please enter your name and we will explain the rules");
-System.out.println("What is your name?");
-String name_user_1 = Input.nextLine();
-System.out.println("Here are the rules," + name_user_1 + ". The rules are
+String name_user_1 = input.nextLine();
+System.out.println("Here are the rules," + name_user_1 + ". The rules are...");
