7397e0fd1040f4a4c9147564a31948cf83a90900
tuck-david <32722002+tuck-david@users.noreply.github.com>
2017-11-02 12:17:33 -0400

    Update game-of-nim-main.java

--- game-of-nim-main.java
+++ game-of-nim-main.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+class sort2 {
+ public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
 System.out.println("Welcome to the game of nim, please enter your name and we will explain the rules");
 System.out.println("What is your name?");
 String name_user_1 = Input.nextLine();
