33155da13cc6ab2562ba1ee1255f56945e91b7a4
tuck-david <32722002+tuck-david@users.noreply.github.com>
2017-11-07 11:47:41 -0500

    Update game-of-nim-main.java

--- game-of-nim-main.java
+++ game-of-nim-main.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 class game-of-nim-main {
  public static void main(String[] args) {
    Scanner input = new Scanner(System.in);
+   Random r = new Random();
 System.out.println("Welcome to the game of nim, please enter your name and we will explain the rules");
 String name_user_1 = input.nextLine();
 System.out.println("Here are the rules," + name_user_1 + ". The rules are...");
+System.out.println("Do you want to play against computer our against a second player"); 
